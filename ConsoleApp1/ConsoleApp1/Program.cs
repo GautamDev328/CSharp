921ed2302fdc7f95e78985d0212c16645925d748
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-08-21 20:04:46 +0530

    cube

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       
+        static void uppercase(string name)
+        {
+           // string name = Console.ReadLine();
+            Console.WriteLine("chamge uppercase :" + name);
+
+
+        }
+      public   static void Main(String[] args)
+        {
+            Program p = new Program();
+            p.uppercase();
+           // Console.WriteLine(p)
+
+
+    }
+    }
+}
---
+++ Cube using whileloop/Cube using whileloop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube_using_whileloop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ctrl = 0;
+            int num;
+            int count = 0;
+            while (ctrl < 5)
+            {
+                Console.WriteLine("Enter a Number:" );
+                num = int.Parse(Console.ReadLine());
+                if (num * num * num > 100)
+                    count++;
+                ctrl = ctrl + 1;
+            }
+            Console.WriteLine("cube : "+ count );
+            Console.ReadLine();
+        }
+    }
+}
