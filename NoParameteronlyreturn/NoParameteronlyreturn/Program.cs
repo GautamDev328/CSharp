786d239c1a6de04e50a79e49776e1a7db2ad1530
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-08-21 20:21:39 +0530

    C#program

---
+++ NoParameteronlyreturn/NoParameteronlyreturn/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoParameteronlyreturn
+{
+    class Program
+    {
+        static string print()
+        {
+            string res;
+            Console.Write(" Enter the Number: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+                res = "Number  is Even";
+            else
+                res = "Number  is Odd";
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            string s = print();
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ forloopprogram/forloopprogram/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forloopprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter a  Number: ");
+            int i = int.Parse(Console.ReadLine());
+            //  int j = int.Parse(Console.ReadLine());
+            /*  for( ; i <= 100; i++)
+              {
+                  Console.WriteLine(i);
+              }
+              Console.ReadLine();*/// 1 to 100 number serialwise print 
+            /* for (; i<=20; i = i + 2)
+             {
+                 Console.WriteLine(i);
+
+             }
+             Console.ReadLine();*/
+            /*  for ( ; i<= 10;++ i)
+              {
+                  for (int j=1 ; j<= i;++ j)
+                  {
+                      Console.Write("*");
+
+                  }
+                  Console.WriteLine();
+
+              }
+
+              Console.ReadLine();*/
+            /*  for( ; i >= 1; i--)
+              {
+                  for (int j = 1; j<=i ; j++)
+                  {
+                      Console.Write("*");
+
+                  }
+                  Console.WriteLine();
+
+              }
+
+              Console.ReadLine();
+          }*/
+            for (; i <= 10; i++)
+            {
+                Console.WriteLine(10 * 10 * 10);
+            }
+            Console.ReadLine();
+            
+    }
+    }
+}
