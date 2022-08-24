d820900b988509c77ba1d00674207843dea21b62
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-08-24 15:50:50 +0530

    swap program

---
+++ swap program/swap program/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            Console.WriteLine("  Before  the progoram a= "+a+"b="+b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+           // b = a + b;
+            Console.WriteLine("After the program a ="+a+"b ="+b);
+            Console.ReadLine();
+        }
+       
+    }
+}
