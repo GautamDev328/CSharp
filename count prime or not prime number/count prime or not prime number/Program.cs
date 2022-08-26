9b7cce2f4f9cf968cda818874443327f9dee8e54
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-08-26 17:30:04 +0530

    Prime or not prime program

---
+++ count prime or not prime number/count prime or not prime number/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace count_prime_or_not_prime_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Array Number: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            int prime = 0;
+            int notprime = 0;
+            int i = 1;
+            int k = 0;Boolean chek = true;int kk = 0;int fix = 0;
+            while (i <= n)
+            {
+                Console.WriteLine("Enter a number: ");
+                a[k] = int.Parse(Console.ReadLine());
+               
+
+                if (a[k] == 1 || a[k] == 1)
+                {
+                    fix++;
+                    i++;
+                    continue;
+                }
+                else
+                {
+                    int count = 1; notprime = 0;
+                    while (count <= a[k])
+                    {
+                        if (a[k] % count == 0)
+                        {
+                            notprime++;
+                        }
+                        count++;
+                    }
+                    if (notprime == 2)
+                    {
+                        prime++;
+                    }
+                }
+                i++;
+                
+
+            }
+
+            Console.WriteLine("NotPrime " + (n - prime - fix));
+            Console.WriteLine("Prime :" + prime);
+            Console.ReadLine();
+
+
+        }
+    }
+}
