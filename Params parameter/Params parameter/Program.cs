086ce305006f4a84bf780ee8774ca08178f8ab9d
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-09-01 16:25:31 +0530

    Params_parameter

---
+++ Params parameter/Params parameter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params_parameter
+{
+    class Program
+    {
+         public static void Distance( params int[] arr)
+        {
+            Console.WriteLine("check the array length:  "+ arr.Length);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("array number: "+" "+ arr[i]);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Distance(1, 2, 3, 45, 54);
+            Console.ReadLine();
+        }
+    }
+}
