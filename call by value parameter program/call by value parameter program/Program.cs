8dec87e40307a6f8c48b23717e8a821a4fd3d759
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-09-01 16:10:16 +0530

    Number :10 20 Swap: 20 10

--- call by value parameter program/call by value parameter program/Program.cs
+++ call by value parameter program/call by value parameter program/Program.cs
@@ -13,16 +13,21 @@ namespace call_by_value_parameter_program
             int z = x;
             x = y;
             y = z;
+            Console.WriteLine("Swap: "+ x+" "+y);
 
         }
         static void Main(string[] args)
         {
             int x = 10;
             int y = 20;
-            Console.WriteLine(x+" "+y);
+            Console.WriteLine("Number :"+ x+" "+y);
             show(x, y);
-            Console.WriteLine(x+" "+y);
+          //  Console.WriteLine(x+" "+y);
             Console.ReadLine();
         }
     }
 }
+/* output:
+ * Number :10 20
+Swap: 20 10
+*/
