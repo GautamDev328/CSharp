5035b51a862e3088e75a71c8ae1acf55d8c597e9
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-09-01 15:53:39 +0530

    call_by_value_parameter_program

---
+++ call by value parameter program/call by value parameter program/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace call_by_value_parameter_program
+{
+    class Program
+    {
+        static void show( int x, int y)
+        {
+            int z = x;
+            x = y;
+            y = z;
+
+        }
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            Console.WriteLine(x+" "+y);
+            show(x, y);
+            Console.WriteLine(x+" "+y);
+            Console.ReadLine();
+        }
+    }
+}
