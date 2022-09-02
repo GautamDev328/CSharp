951bd22425c7407607bb702ab31945bbbdd7750e
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-09-02 16:20:16 +0530

    Simple programs  and SQL QUERY

---
+++ Simple programs/Simple programs/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_programs
+{
+    class Program
+    {
+        public int x = 10;
+        public int y = 20;
+        public void sum()
+        {
+            Console.WriteLine("sum:" +( x + y));
+        }
+        public void sub()
+        {
+            Console.WriteLine("sub:" + (x - y));
+        }
+    }
+    class Demo { 
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.sum();
+            p.sub();
+           
+
+          
+            Console.WriteLine("Add: "+ (p.x+p.y));
+            Console.WriteLine("minus: "+ (p.x - p.y));
+            Console.WriteLine("miult: "+(p.x *p.y));
+            Console.WriteLine("Div: " + (p.x / p.y));
+            Console.ReadLine();
+
+        }
+    }
+}
+//sum: 30
+//sub: -10
+//Add: 30
+//minus: -10
+
