e2c03e7cf6b99d00ea7805b12d025084be4c517c
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-09-05 21:00:40 +0530

    Add files via upload

---
+++ constructornormal progra/constructornormal progra/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructornormal_progra
+{
+   public  class Program
+    {
+        public void sum()
+        {
+            Console.WriteLine("Hi");
+        }
+        public void sum(int x, int y)
+        {
+            Console.WriteLine(x+y);
+        }
+        public void sum(int x, int y , int z)
+        {
+            Console.WriteLine(x+y+z);
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.sum();
+            p.sum(20, 10);
+            p.sum(20, 10, 5);
+            Console.ReadLine();
+                
+        }
+    }
+}
