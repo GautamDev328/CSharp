ae12fa57593ed995d6b4182d1bf899d5dd0db68e
GAUTAM DEV <73927189+GautamDev328@users.noreply.github.com>
2022-09-07 23:43:12 +0530

    Hierarchical  inheritance program

---
+++ Hierarchical inheritance program/Hierarchical inheritance program/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hierarchical_inheritance_program
+{
+    class Program
+    {
+        protected void show()
+        {
+            Console.WriteLine("father");
+
+        }
+
+        class Demo : Program
+        {
+            public void show1()
+            {
+               show();
+                Console.WriteLine("chile-1");
+            }
+        }
+        class System : Demo
+        {
+            public void show1()
+            {
+                show();
+                Console.WriteLine("child-2");
+            }
+
+
+        }
+        class Mummy
+        {
+
+            static void Main(string[] args)
+            {
+                Demo d = new Demo();
+                System s = new System();
+                d.show1();
+                s.show1();
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
